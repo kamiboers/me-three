b96d8f8a29b36550b70b9d7ffb6177ff28247bbe
Kami Boers <kamiboers@gmail.com>
2016-10-24 15:08:59 -0600

    navbar

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 $( document ).ready(function() {
 
-}
+  $(".parallax").scroll(function() {      
+    if ($('.parallax').scrollTop() > 10) {        
+      $('.navbar').removeClass('navbar-transparent').addClass('navbar-opaque').css('padding-top', '0px');
+      // $('#navbar-icon-img').attr('src', './images/logo-sova-sm.png');
+    } else {
+      $('.parallax__layer--back').toggle().toggle();
+      $('.navbar').addClass('navbar-transparent').removeClass('navbar-opaque').css('padding-top', '25px');
+      // $('#navbar-icon-img').attr('src', './images/logo-full.png');
+    }
+  });
+
+});
