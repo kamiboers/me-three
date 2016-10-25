cc98ff8a8547a3a2d7a0e43c33cfb02acb7019aa
Kami Boers <kamiboers@gmail.com>
2016-10-24 20:57:45 -0600

    splash and hero

--- js/index.js
+++ js/index.js
@@ -2,13 +2,28 @@ $( document ).ready(function() {
 
   $(".parallax").scroll(function() {      
     if ($('.parallax').scrollTop() > 10) {        
-      $('.navbar').removeClass('navbar-transparent').addClass('navbar-opaque').css('padding-top', '0px');
-      // $('#navbar-icon-img').attr('src', './images/logo-sova-sm.png');
+      $('.navbar').removeClass('navbar-transparent').addClass('navbar-opaque');
+      $('.brand-image').css('width', '200px');
     } else {
       $('.parallax__layer--back').toggle().toggle();
-      $('.navbar').addClass('navbar-transparent').removeClass('navbar-opaque').css('padding-top', '25px');
-      // $('#navbar-icon-img').attr('src', './images/logo-full.png');
+      $('.navbar').addClass('navbar-transparent').removeClass('navbar-opaque');
+      $('.brand-image').css('width', '260px');
     }
   });
+
+  $('.hero').addClass('fadeIn');
+  $('.brand-image').addClass('dropIn');
+  $('.navbar-links > ul > li').addClass('dropIn');
+
+  $('.page-down-button').hover(function() {
+    $('.hero > h1').css('color', '#023364');
+  }, function() {
+    $('.hero > h1').css('color', '#02264b');
+  })
+
+  $('.page-down-button').click(function() {
+    $('.parallax').animate({scrollTop:$(".content-opaque").offset().top}, 1000);
+  })
+
 
 });
